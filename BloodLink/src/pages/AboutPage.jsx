7cfebaa6dc126de65f37185d77d06834d5eed851
Kaburu <kabmunene@gmail.com>
2026-06-12 12:01:22 +0300

    completion of the homepage

---
+++ BloodLink/src/pages/AboutPage.jsx
@@ -0,0 +1,5 @@
+export default function AboutPage() {
+    return(
+        <p className="word">hello</p>
+    )
+}
---
+++ BloodLink/src/pages/auth/LoginPage.jsx
@@ -0,0 +1,5 @@
+export default function LoginPage() {
+    return(
+        <p className="word">hello</p>
+    )
+}
